b7dd5d4682272c1c95033fd7785be0baf5928d53
wangyongf <scottwang1996@gmail.com>
2016-09-01 16:22:10 +0800

    first commit

---
+++ src/com/yongf/compiler/FileReader.java
@@ -0,0 +1,11 @@
+package com.yongf.compiler;
+
+/**
+ * Created by scottwang on 16-9-1.
+ */
+public class FileReader {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
